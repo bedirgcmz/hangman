4bb77cb11ae17b874d3deb71affb6d459d644afa
bedirgcmz <bdrgcmz@gmail.com>
2023-11-26 20:47:37 +0100

    keydoen function writed

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 /** DOM element */
 const wordArea = document.querySelector(".word-area")
+const popupContainer = document.querySelector(".popup-container")
+const popupBackground = document.querySelector(".popup-background")
 
-
+/**  Variables */
+const correctLetters = []
+const wrongLetters = []
 
 const getRandomWord = () => {
     const wordList = ["Javascript", "java", "css"]
     return wordList[Math.floor(Math.random() * wordList.length)]
 }
+const selectedWord = getRandomWord();
 
-const correctLetters = ['j', 'a']
 const displayWord = () => {
-    const selectedWord = getRandomWord();
 
     wordArea.innerHTML = `
     ${selectedWord.split('').map(letter => `
@@ -18,11 +21,31 @@ const displayWord = () => {
     `).join('')}
     `;
 
-    const guessWord = wordArea.innerText(/\n/g, '');
+    const guessWord = wordArea.innerText.replace(/\n/g, '');
     if (guessWord === selectedWord) {
-        console.log("win")
+        popupContainer.style.display =  'flex';
+        popupBackground.style.display =  'flex';
     }
 }
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+        const enteredLetter = e.key;
+
+        if (selectedWord.includes(enteredLetter)) {
+            if (!correctLetters.includes(enteredLetter)) {
+                correctLetters.push(enteredLetter)
+                displayWord();
+            }else {
+                console.log('tiklandi');
+            }   
+        } else {
+            wrongLetters.push(enteredLetter)
+            console.log('hatali harfleri guncelle')
+        }
+    } 
+})
+
 
 displayWord()
 
