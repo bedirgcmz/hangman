3fe5ad5d8abcb560b81c3b8f6947d2a33fca3dae
bedirgcmz <bdrgcmz@gmail.com>
2023-11-26 22:38:57 +0100

    game complated. Version 1 is ready

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 /** DOM element */
 const wordArea = document.querySelector(".word-area")
 const popupContainer = document.querySelector(".popup-container")
+const popup = document.querySelector(".popup")
+const successMessage = document.querySelector(".success-message")
 const popupBackground = document.querySelector(".popup-background")
+const errorArea = document.querySelector(".error-area")
+const partOfPicture = document.querySelectorAll(".part-of-picture")
+const repeatLetter = document.getElementById('repeat-letter')
+const playAgainButton = document.querySelector(".play-again")
 
 /**  Variables */
 const correctLetters = []
 const wrongLetters = []
 
 const getRandomWord = () => {
-    const wordList = ["Javascript", "java", "css"]
+    const wordList = ["javascript", "java", "css", "html", "bootstrap", "react", "angular"]
     return wordList[Math.floor(Math.random() * wordList.length)]
 }
-const selectedWord = getRandomWord();
+let selectedWord = getRandomWord();
 
 const displayWord = () => {
 
@@ -23,33 +29,79 @@ const displayWord = () => {
 
     const guessWord = wordArea.innerText.replace(/\n/g, '');
     if (guessWord === selectedWord) {
+        successMessage.innerText = 'Congratulations, You Won';
+        popup.classList.remove('lost')
+        popup.classList.add('win')
         popupContainer.style.display =  'flex';
         popupBackground.style.display =  'flex';
     }
 }
 
+const updateWrongLetter = () => {
+    errorArea.innerHTML = `
+    ${wrongLetters.length > 0 ? `<h3>Wrong Letter</h3>`: ''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`).join(' - ')}
+    `
+    partOfPicture.forEach((item, index) => {
+        const errorCount = wrongLetters.length;
+        if (index < errorCount) {
+            item.style.display = 'block'
+        } else {
+            item.style.display = 'none'
+        }
+    })
+    
+    if (wrongLetters.length === partOfPicture.length) {
+        successMessage.innerText = "I'm sorry, you lost";
+        popup.classList.remove('win')
+        popup.classList.add('lost')
+        popupContainer.style.display =  'flex';
+        popupBackground.style.display =  'flex';
+        
+    }
+
+}
+
+const repeatLetterMessage = () => {
+    repeatLetter.classList.add('show')
+
+    setTimeout (() => {
+    repeatLetter.classList.remove('show')
+    }, 2000)
+}
+
+playAgainButton.addEventListener("click", () => {
+    correctLetters.splice(0)
+    wrongLetters.splice(0)
+    selectedWord = getRandomWord()
+    displayWord()
+    updateWrongLetter()
+    popupContainer.style.display = 'none'
+    popupBackground.style.display = 'none'
+})
+
 window.addEventListener('keydown', function (e) {
     if (e.keyCode >= 65 && e.keyCode <= 90) {
-        const enteredLetter = e.key;
+        const enteredLetter = (e.key).toLowerCase();
 
         if (selectedWord.includes(enteredLetter)) {
             if (!correctLetters.includes(enteredLetter)) {
                 correctLetters.push(enteredLetter)
                 displayWord();
             }else {
-                console.log('tiklandi');
+                repeatLetterMessage()
             }   
         } else {
-            wrongLetters.push(enteredLetter)
-            console.log('hatali harfleri guncelle')
+            if (!wrongLetters.includes(enteredLetter)){
+                wrongLetters.push(enteredLetter)
+                updateWrongLetter()
+            }else {
+                repeatLetterMessage()
+
+            }   
         }
     } 
 })
 
 
 displayWord()
-
-
-console.log(getRandomWord());
-
-
